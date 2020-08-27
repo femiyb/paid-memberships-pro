b93d4b635f837f5d3babec280aee596a915d6c1c
Femi <94sam.fem@gmail.com>
2020-08-27 12:11:26 +0200

    Update pmpro-stripe.js

--- js/pmpro-stripe.js
+++ js/pmpro-stripe.js
@@ -6,7 +6,7 @@ jQuery( document ).ready( function( $ ) {
 	var stripe, elements, cardNumber, cardExpiry, cardCvc;
 
 	// Identify with Stripe.
-	stripe = Stripe( pmproStripe.publishableKey );
+	stripe = Stripe( pmproStripe.publishableKey, {locale: 'ja'} );
 	elements = stripe.elements();
 
 	// Create Elements.
